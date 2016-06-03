6cee83a79d648408d874be6a03af46a5aed4d1ba
Barrett Lewis <musikal.fusion@gmail.com>
2016-06-03 16:37:04 -0700

    Adds uploader

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from flask import Flask, request
+from flask import Flask, request, send_file
 from graphviz import Source
 from base64 import urlsafe_b64encode, urlsafe_b64decode
 import pydot
@@ -14,6 +14,7 @@ def graph_upload():
     file_contents = file_vals.stream.read().decode('utf-8')
     return Source(file_contents).pipe(format='svg')
 
+
 @app.route('/get-page', methods=['POST'])
 def graph_to_page():
     file_vals = next(request.files.values())
@@ -23,6 +24,7 @@ def graph_to_page():
     encoded = urlsafe_b64encode(compressed)
     return request.host_url + "view?s=" + encoded.decode('utf-8')
 
+
 @app.route('/view', methods=['GET'])
 def view():
     source = urlsafe_b64decode(request.args['s'])
@@ -31,7 +33,14 @@ def view():
     return Source(source).pipe(format='svg')
 
 
-
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file_vals = next(request.files.values())
+        file_contents = file_vals.stream.read().decode('utf-8')
+        return Source(file_contents).pipe(format='svg')
+    else:
+        return send_file('upload.html')
 
 
 if __name__ == '__main__':
