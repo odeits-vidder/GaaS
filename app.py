8dba07981f8055c012dae80a4b86fad92cc2a1b8
Barrett Lewis <musikal.fusion@gmail.com>
2016-06-03 15:55:54 -0700

    better support for large files Adds compression Adds parser that sort of minituraizes it?

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from flask import Flask, request
 from graphviz import Source
+from base64 import urlsafe_b64encode, urlsafe_b64decode
+import pydot
+import zlib
 
 app = Flask(__name__)
 
@@ -10,6 +13,26 @@ def graph_upload():
     file_vals = next(request.files.values())
     file_contents = file_vals.stream.read().decode('utf-8')
     return Source(file_contents).pipe(format='svg')
+
+@app.route('/get-page', methods=['POST'])
+def graph_to_page():
+    file_vals = next(request.files.values())
+    file_contents = file_vals.stream.read().decode('utf-8')
+    file_contents = pydot.graph_from_dot_data(file_contents).to_string()
+    compressed = zlib.compress(file_contents.encode('utf-8'), 9)
+    encoded = urlsafe_b64encode(compressed)
+    return request.host_url + "view?s=" + encoded.decode('utf-8')
+
+@app.route('/view', methods=['GET'])
+def view():
+    source = urlsafe_b64decode(request.args['s'])
+    source = zlib.decompress(source)
+    source = source.decode('utf-8')
+    return Source(source).pipe(format='svg')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
